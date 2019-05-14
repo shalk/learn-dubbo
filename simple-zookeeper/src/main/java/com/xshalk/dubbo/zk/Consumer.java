5fb993a577c77ac2afccab1ab8c8dcb502796e70
shalk <xshalk@163.com>
2019-05-14 11:10:45 +0800

    update

---
+++ simple-zookeeper/src/main/java/com/xshalk/dubbo/zk/Consumer.java
@@ -0,0 +1,18 @@
+package com.xshalk.dubbo.zk;
+
+import com.xshalk.dubbo.simple.api.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author xiaokun
+ * @since 19-5-8
+ */
+public class Consumer {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"consumer.xml"});
+        context.start();
+        DemoService demoService = (DemoService) context.getBean("bean1");
+        String hello = demoService.sayHello("zookeeper" );
+        System.out.println(hello);
+    }
+}
---
+++ simple-zookeeper/src/main/java/com/xshalk/dubbo/zk/DemoServiceImpl.java
@@ -0,0 +1,18 @@
+package com.xshalk.dubbo.zk;
+
+import com.xshalk.dubbo.simple.api.DemoService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author xiaokun
+ * @since 19-5-7
+ */
+public class DemoServiceImpl implements DemoService {
+    public static Log LOGGER = LogFactory.getLog(DemoServiceImpl.class);
+
+    public String sayHello(String name) {
+        LOGGER.info("say hello" + name);
+        return "hello " + name;
+    }
+}
